e6455170ee00393767ea82c3a83f39b43c93a201
satyamgitt <satyampriyam98@gmail.com>
2023-09-30 12:58:24 +0530

    gitaccount

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ const app = express()
 // !port
 const port = 4000
 
+// !DATA
+const gitHub = {
+    "login": "satyamgitt",
+    "id": 100476592,
+    "node_id": "U_kgDOBf0msA",
+    "avatar_url": "https://avatars.githubusercontent.com/u/100476592?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/satyamgitt",
+    "html_url": "https://github.com/satyamgitt",
+    "followers_url": "https://api.github.com/users/satyamgitt/followers",
+    "following_url": "https://api.github.com/users/satyamgitt/following{/other_user}",
+    "gists_url": "https://api.github.com/users/satyamgitt/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/satyamgitt/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/satyamgitt/subscriptions",
+    "organizations_url": "https://api.github.com/users/satyamgitt/orgs",
+    "repos_url": "https://api.github.com/users/satyamgitt/repos",
+    "events_url": "https://api.github.com/users/satyamgitt/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/satyamgitt/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Satyam rao",
+    "company": null,
+    "blog": "",
+    "location": "Noida",
+    "email": null,
+    "hireable": null,
+    "bio": "I am FRONTEND DEVELOPER",
+    "twitter_username": "Satyamrao",
+    "public_repos": 20,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 4,
+    "created_at": "2022-02-26T13:41:00Z",
+    "updated_at": "2023-09-25T18:46:27Z"
+    }
 
 
 // ! get request {home route and callback function}
@@ -23,6 +58,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/youtube" , (req , res)=>{
 res.send(`<h1>Hello Bro HTML</h1>`)
+})
+
+app.get("/github" , (req , res)=>{
+res.json(gitHub)
 })
 
 app.listen(port, () => {
