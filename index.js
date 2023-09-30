54e76f89dd35665ceea0b5fd0b4a321daa82640c
satyamgitt <satyampriyam98@gmail.com>
2023-09-30 12:48:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// !made env variable for production ready app
+require('dotenv').config()
+
+const express = require('express')
+
+// !main app variable
+const app = express()
+
+// !port
+const port = 4000
+
+
+
+// ! get request {home route and callback function}
+
+console.log(process.env) /
+app.get("/", (req, res) => {
+    res.send("Lo Bhai Satyam Pahela app ban gya")
+})
+
+app.get("/twitter", (req, res) => {
+    res.send("Jalwa hai Bhai ka ")
+})
+
+app.get("/youtube" , (req , res)=>{
+res.send(`<h1>Hello Bro HTML</h1>`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
